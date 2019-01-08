f8ca841410e33638e4ffa8b919cdff89c8996c09
“Simaxa7” <simaxa7@mail.ru>
2019-01-09 01:05:26 +0300

    feat: add game

---
+++ src/js/characters/backGroundImg.js
@@ -0,0 +1,10 @@
+const backGroundImg = {
+  bg: [
+    './src/img/backGround/city_1.png',
+    './src/img/backGround/city_2.png',
+    './src/img/backGround/city_3.png',
+    './src/img/backGround/city_4.png',
+  ],
+};
+
+export default backGroundImg;
---
+++ src/js/characters/kennyBodyParts.js
@@ -0,0 +1,13 @@
+const kennyBodyParts = {
+  body: [
+    './src/img/body_parts/kenny_body_1.png',
+    './src/img/body_parts/kenny_body_3.png',
+  ],
+  head: [
+    './src/img/body_parts/kenny_head_1.png',
+    './src/img/body_parts/kenny_head_2.png',
+    './src/img/body_parts/kenny_head_3.png',
+  ],
+};
+
+export default kennyBodyParts;
