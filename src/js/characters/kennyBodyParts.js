e17efeede205f0ac1816a6fab199bea4235af3ef
“Simaxa7” <simaxa7@mail.ru>
2019-01-09 14:28:33 +0300

    fix rename my picture

--- src/js/characters/kennyBodyParts.js
+++ src/js/characters/kennyBodyParts.js
@@ -1,7 +1,7 @@
 const kennyBodyParts = {
   body: [
     './src/img/body_parts/kenny_body_1.png',
-    './src/img/body_parts/kenny_body_3.png',
+    './src/img/body_parts/kenny_body_2.png',
   ],
   head: [
     './src/img/body_parts/kenny_head_1.png',
