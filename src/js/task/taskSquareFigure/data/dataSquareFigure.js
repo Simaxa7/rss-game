0929a08ba38e695f356a00934b6b4f178597a9cb
“Simaxa7” <simaxa7@mail.ru>
2019-01-09 16:04:39 +0300

    fix chapge path in SqureFigure

--- src/js/task/taskSquareFigure/data/dataSquareFigure.js
+++ src/js/task/taskSquareFigure/data/dataSquareFigure.js
@@ -1,18 +1,18 @@
 const dataSquareFigure = [
   {
-    figureImg: './src/img/imgTasks/imgTaskSquareFigure/circle.png',
+    figureImg: './img/imgTasks/imgTaskSquareFigure/circle.png',
     description: 'круг',
     param: 'radius',
     formula: '3.14*radius*radius',
   },
   {
-    figureImg: './src/img/imgTasks/imgTaskSquareFigure/square.png',
+    figureImg: './img/imgTasks/imgTaskSquareFigure/square.png',
     description: 'квадрат',
     param: 'sideSquare',
     formula: 'sideSquare*sideSquare',
   },
   {
-    figureImg: './src/img/imgTasks/imgTaskSquareFigure/triangle.png',
+    figureImg: './img/imgTasks/imgTaskSquareFigure/triangle.png',
     description: 'прямоугольный равнобедренный треугольник',
     param: 'sideTriangle',
     formula: 'sideTriangle*sideTriangle/2',
