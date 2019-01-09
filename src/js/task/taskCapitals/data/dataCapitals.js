7d8c9cb13685c12e66131e138b3e8980fd649641
“Simaxa7” <simaxa7@mail.ru>
2019-01-09 16:07:04 +0300

    fix chapge path in Capitals

--- src/js/task/taskCapitals/data/dataCapitals.js
+++ src/js/task/taskCapitals/data/dataCapitals.js
@@ -1,19 +1,19 @@
 const dataCapitals = [
   {
     capital: 'оттава',
-    img: './src/img/imgTasks/imgTaskCapitals/canada.png',
+    img: './img/imgTasks/imgTaskCapitals/canada.png',
   },
   {
     capital: 'пекин',
-    img: './src/img/imgTasks/imgTaskCapitals/china.png',
+    img: './img/imgTasks/imgTaskCapitals/china.png',
   },
   {
     capital: 'берлин',
-    img: './src/img/imgTasks/imgTaskCapitals/germany.png',
+    img: './img/imgTasks/imgTaskCapitals/germany.png',
   },
   {
     capital: 'вашингтон',
-    img: './src/img/imgTasks/imgTaskCapitals/usa.png',
+    img: './img/imgTasks/imgTaskCapitals/usa.png',
   },
 ];
 
